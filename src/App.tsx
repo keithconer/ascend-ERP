5b5aaa330dfd236df08dba632dfb091246e8fe70
Markewww <154045396+Markewww@users.noreply.github.com>
2025-09-10 21:37:59 +0800

    Initial Commit (Module 9)

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import InventoryManagement from "./pages/inventory/InventoryManagement";
+import ProjectManagement from "./pages/projects/ProjectManagement"; // Module 9 - Project Management
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -18,6 +19,7 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/inventory" element={<InventoryManagement />} />
+          <Route path="/projects" element={<ProjectManagement />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
